d6bbd1c8be7b73aaf6d374ff9dc5b04006d539ed
byronsc1711 <byrondavid19@hotmail.com>
2020-06-25 20:43:34 -0500

    Primer Commit

---
+++ ConsoleTarea1/Program.cs
@@ -0,0 +1,44 @@
+﻿//Byron Guaygua
+
+using System;
+
+namespace ConsoleTarea1
+{
+    class Cuadrado
+    {
+        private int lado;
+        public void Inicializar()
+        {
+            Console.Write("Ingrese el valor numérico de el lado :");
+            string linea;
+            linea = Console.ReadLine();
+            lado = int.Parse(linea);
+        }
+
+        //uso del metodo para que imprima el valor del perimetro del cuadrado
+        public void ImprimirPerimetro()
+        {
+            int perimetro;
+            perimetro = lado * 4;
+            Console.WriteLine("El perímetro del cuadrado es:" + perimetro);
+        }
+
+        //uso del metodo para que imprima el valor de la superficie del cuadrado
+        public void ImprimirSuperficie()
+        {
+            int superficie;
+            superficie = lado * lado;
+            Console.WriteLine("La superficie del cuadrado es:" + superficie);
+        }
+
+        static void Main(string[] args)
+        {
+            Cuadrado cuadradoFinal = new Cuadrado();
+            cuadradoFinal.Inicializar();
+            cuadradoFinal.ImprimirPerimetro();
+            cuadradoFinal.ImprimirSuperficie();
+            Console.ReadKey();
+        }
+    }
+}
+
